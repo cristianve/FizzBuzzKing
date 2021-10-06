349e2b788ddd5b6825a59c09cc5b3c3c6ef3192f
unknown <97cvega@gmail.com>
2021-10-06 20:50:39 +0200

    first approach without threads

--- FizzBuzz/src/main/FizzBuzz.java
+++ FizzBuzz/src/main/FizzBuzz.java
@@ -6,24 +6,50 @@ public class FizzBuzz {
 
     FizzBuzz(int n) {
         this.n = n;
-
     }
 
     void fizz() {
+        System.out.println("fizz");
         // printFizz() outputs "fizz".
     }
 
     void buzz() {
+        System.out.println("buzz");
         // printBuzz() outputs "buzz".
     }
 
     void fizzbuzz() {
+        System.out.println("fizzbuzz");
         // printFizzBuzz() outputs "fizzbuzz".
     }
 
-    void number() {
+    void number(int i) {
+        System.out.println(i);
         // printNumber(x) outputs "x", where x is an integer.
     }
 
+
+    public static void main(String[] args) {
+        int n = 15;
+        FizzBuzz fizzBuzz = new FizzBuzz(n);
+        fizzBuzz.init();
+    }
+
+    private void init() {
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                fizzbuzz();
+            } else if (i % 3 == 0 && i % 5 != 0) {
+                fizz();
+            } else if (i % 3 != 0 && i % 5 == 0) {
+                buzz();
+            } else {
+                number(i);
+            }
+            this.n = n++;
+        }
+
+    }
 
 }
